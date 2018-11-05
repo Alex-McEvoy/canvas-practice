ba15644fdb163acf80b085aae72ecf628d32a210
TangledTessellations <mcevoya@oregonstate.edu>
2018-11-04 17:38:34 -0800

    Finish lesson 3 in chris Course on animating in Canvas https://www.youtube.com/watch?v=yq2au9EfeRQ

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -42,3 +42,58 @@ for (let i = 0; i < Math.random() * 10; i++) {
     c.strokeStyle = randomColor();
     c.stroke();
 }
+
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = randomColor();
+
+    this.draw = function () {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = this.color;
+        c.fillStyle = this.color;
+        c.stroke();
+        c.fill();
+    }
+
+    this.update = function () {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+            this.color = randomColor();
+        }
+
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+            this.color = randomColor();
+        }
+        this.x += this.dx;
+        this.y += this.dy
+        this.draw()
+    }
+}
+
+let circles = [];
+
+for (let i = 0; i < Math.random() * 300; i++) {
+    let radius = Math.random() * 30;
+    let x = (Math.random() * (innerWidth - radius * 2)) + radius;
+    let y = (Math.random() * (innerHeight - radius * 2)) + radius;
+    let dx = (Math.random() - 0.5) * 8;
+    let dy = (Math.random() - 0.5) * 8;
+
+    circles.push(new Circle(x, y, dx, dy, radius));
+
+}
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
+    circles.forEach(circle => circle.update());
+}
+
+animate();
